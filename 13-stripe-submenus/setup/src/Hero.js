e82e299f8580f17d28afb2823c23d9abaad8aca6
Wojciech Kozieł <wojtek98koziel@gmail.com>
2021-08-21 23:02:05 +0200

    Task 13 completed

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import { useGlobalState } from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const { closeSubmenu } = useGlobalState()
+  return (
+    <section className='hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>
+            Payments infrastructure <br />
+            for the internet
+          </h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt='phone img' className='phone-img' />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,54 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import { useGlobalState } from './context'
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  const { closeSubmenu, openSubmenu } = useGlobalState()
+
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent
+    const tempBtn = e.target.getBoundingClientRect()
+    const center = (tempBtn.left + tempBtn.right) / 2
+    const bottom = tempBtn.bottom - 3
+    openSubmenu(page, { center, bottom })
+  }
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubmenu()
+    }
+  }
+
+  return (
+    <nav className='nav' onMouseOver={handleSubmenu}>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='stripe' />
+          <button className='brn toggle-btn' onClick={displaySubmenu}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        <button className='signin-btn btn'>sign in</button>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
