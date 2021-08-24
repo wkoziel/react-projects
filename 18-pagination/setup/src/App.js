d527588417e730a779af68396b04275c7cbc94ed
Wojciech Kozieł <wojtek98koziel@gmail.com>
2021-08-24 19:11:13 +0200

    Task 18 completed

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -1,8 +1,66 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
+
 function App() {
-  return <h2>pagination starter</h2>
+  const [followers, setFollowers] = useState([])
+  const [page, setPage] = useState(0)
+
+  const { loading, data } = useFetch()
+
+  useEffect(() => {
+    if (loading) return
+    setFollowers(data[page])
+  }, [loading, page])
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      if (oldPage - 1 < 0) return 0
+      return oldPage - 1
+    })
+  }
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      if (oldPage + 1 > data.length - 1) return 0
+      return oldPage + 1
+    })
+  }
+
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'Loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
+      </div>
+      <section className='followers'>
+        <div className='container'>
+          {followers.map((follower) => (
+            <Follower key={follower.id} {...follower} />
+          ))}
+        </div>
+
+        {!loading && (
+          <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              Prev
+            </button>
+            {data.map((_, index) => (
+              <button
+                className={`page-btn ${index === page ? 'active-btn' : null}`}
+                onClick={() => setPage(index)}
+              >
+                {index + 1}
+              </button>
+            ))}
+            <button className='next-btn' onClick={nextPage}>
+              Next
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
+  )
 }
 
 export default App
--- 18-pagination/setup/src/Follower.js
+++ 18-pagination/setup/src/Follower.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const Follower = () => {
-  return <h2>follower component</h2>
+const Follower = ({ avatar_url: av, html_url: url, login }) => {
+  return (
+    <article className='card'>
+      <img src={av} alt={login} />
+      <h4>{login}</h4>
+      <a href={url} className='btn'>
+        view profile
+      </a>
+    </article>
+  )
 }
 
 export default Follower
